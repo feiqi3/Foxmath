c246a4e79394df8d76254da68b6586d8df0ea127
feiqi3 <zjgtzn@outlook.com>
2023-06-08 15:02:53 +0800

    Add some macros.

--- base/basic_def.h
+++ base/basic_def.h
@@ -1,16 +1,15 @@
 #ifndef BASIC_DEF_H
 #define BASIC_DEF_H
 
+#include <stdint.h>
 #define FM_DEBUG
 
 //**NOTE** There is only one calling convention under x86-64, a strange fastcall
 
 //Only works under x86
-#if defined(_MSC_VER) && !defined(_M_ARM) && !defined(_M_ARM64)
+#if defined(_MSC_VER)
 #define _FM_USE_VECTORCALL_  
-#endif
-
-#if defined (__GNUC__)
+#elif defined (__GNUC__) || defined (__clang__)
 #define _FM_USE_FASTCALL_ 
 #endif
 
@@ -44,5 +43,58 @@
 #define FM_ALIGNED(x) struct alignas(x)
 
 #define FM_INLINE inline
+
+namespace fm{
+
+namespace simd {
+
+//Default use float as vector's element
+#if !defined (_FM_USE_DOUBLE) && !defined (_FM_USE_FLOAT)
+    #define _FM_USE_FLOAT
+#endif
+
+#if defined(_M_ARM) || defined(_M_ARM64)
+    #error Arm is not supported now
+#endif
+
+#if (defined (_FM_PURE_))
+    #warning SIMD Optimization is closed.
+    #define FM_ALIGN_REQ 8
+#endif
+
+#if defined (_FM_USE_DOUBLE)
+    #define FM_ALIGN_REQ 32
+    #define FMFLOAT double
+    #if defined (_FM_AVX2_)
+        using _fm_vec4 = __m256d;
+    #endif
+#elif defined (_FM_USE_FLOAT)
+    #define FM_ALIGN_REQ 16
+    #define FMFLOAT float
+    #if defined (_FM_SSE4_)
+    using _fm_vec4 = __m128;
+    #endif
+#endif
+
+#if defined (_FM_PURE_)
+using _fm_vec4 = struct {
+        FMFLOAT v[4];
+    };
+#endif
+#include <cassert>
+//Mem align check
+void FM_INLINE FM_CALL MEM_ALIGN_CHECK(const void* ptr,size_t alignment){
+    assert((int64_t)ptr % alignment == 0);
+}
+
+struct alignas(FM_ALIGN_REQ) fmAlignFLoat4{
+    FMFLOAT _v[4];
+    FMFLOAT& operator [](size_t t){
+        return _v[t];
+    }
+
+};
+}
+}
 
 #endif //BASIC_DEF_H
