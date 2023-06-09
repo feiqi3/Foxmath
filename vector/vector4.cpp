efc0f9260be1d582a1e40c694147d8f55f4efeae
feiqi3 <zjgtzn@outlook.com>
2023-06-09 12:33:07 +0800

    Move definitions from .h to .cpp

---
+++ vector/vector4.cpp
@@ -0,0 +1,125 @@
+#include "vector4.h"
+#include <cmath>
+namespace fm {
+vector4::vector4(FMFLOAT *vec) {
+  std::memcpy(this->__data._v, vec, 4 * sizeof(FMFLOAT));
+}
+
+  vector4::vector4(FMFLOAT a){
+    __data[0] = a;
+    __data[1] = a;
+    __data[2] = a;
+    __data[3] = a;
+  }
+
+
+vector4::vector4(FMFLOAT a, FMFLOAT b, FMFLOAT c, FMFLOAT d) {
+  this->__data[0] = a;
+  this->__data[1] = b;
+  this->__data[2] = c;
+  this->__data[3] = d;
+}
+
+const vector4 FM_INLINE FM_CALL
+vector4::operator+(const vector4 &b) const noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+  simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+
+  simd::_fm_vec4 ret = simd::fmVecAdd(veca, vecb);
+  return vector4(ret);
+}
+
+const vector4 FM_INLINE FM_CALL
+vector4::operator-(const vector4 &b) const noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+  simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+
+  simd::_fm_vec4 ret = simd::fmVecSub(veca, vecb);
+  return vector4(ret);
+}
+
+const vector4 FM_INLINE FM_CALL
+vector4::operator*(const vector4 &b) const noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+  simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+  simd::_fm_vec4 ret = simd::fmVecMul(veca, vecb);
+  return vector4(ret);
+}
+
+const vector4 FM_INLINE FM_CALL
+vector4::operator/(const vector4 &b) const noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+  simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+  simd::_fm_vec4 ret = simd::fmVecDiv(veca, vecb);
+  return vector4(ret);
+}
+
+const FMFLOAT FM_INLINE FM_CALL vector4::dot(const vector4 &b) const noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+  simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+  return simd::fmVecDot(veca, vecb);
+}
+
+  const vector4 FM_INLINE FM_CALL vector4::cross(const vector4 &b) const noexcept {
+    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
+    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+    simd::_fm_vec4 ret = simd::fmVec3Cross(veca, vecb);
+    return vector4(ret);
+  }
+
+    FMFLOAT FM_INLINE FM_CALL vector4::maxElement() const noexcept {
+    simd::_fm_vec4 vec = simd::fmLoadVecP(__data._v);
+    return simd::fmMaxElemOfVec(vec);
+  }
+
+    FMFLOAT FM_INLINE FM_CALL vector4::minElement() const noexcept {
+    simd::_fm_vec4 vec = simd::fmLoadVecP(__data._v);
+    return simd::fmMinElemOfVec(vec);
+  }
+
+    FMFLOAT FM_INLINE FM_CALL vector4::length() const noexcept {
+    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
+    FMFLOAT ff = simd::fmVecDot(vec, vec);
+    return std::sqrt(ff);
+  }
+
+    vector4 FM_INLINE FM_CALL vector4::square() const noexcept {
+    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
+    return vector4(simd::fmVecMul(vec, vec));
+  }
+
+    vector4 FM_INLINE FM_CALL vector4::sqrt() const noexcept {
+    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
+    return simd::fmVecSqrt(vec);
+  }
+
+  std::ostream &operator<<(std::ostream &out, const vector4 &s) {
+    out << "(" << s.__data._v[0] << "," << s.__data._v[1] << ","
+        << s.__data._v[2] << "," << s.__data._v[3] << ")";
+    return out;
+  }
+
+  const vector4 FM_INLINE FM_CALL operator*(const vector4 &a,
+                                          FMFLOAT b) noexcept {
+  simd::_fm_vec4 veca = simd::fmLoadVecP(a.__data._v);
+  simd::_fm_vec4 ret = simd::fmVecScale(veca, b);
+  return vector4(ret);
+}
+
+const vector4 FM_INLINE FM_CALL operator/(const vector4 &a,
+                                          FMFLOAT b) noexcept {
+  FMFLOAT inv = ((FMFLOAT)1) / b;
+  return a * inv;
+}
+
+const vector4 FM_INLINE FM_CALL operator*(FMFLOAT b, const vector4 &a) noexcept{
+    return a*b;
+}
+
+bool FM_INLINE FM_CALL hasNan(const vector4& in)noexcept{
+    return std::isnan(in[0]) || std::isnan(in[1]) 
+    || std::isnan(in[2]) || std::isnan(in[3]);
+}
+
+
+} // namespace fm
--- vector/vector4.h
+++ vector/vector4.h
@@ -11,96 +11,40 @@
 namespace fm {
 class vector4 {
 public:
-  vector4(FMFLOAT *vec) {
-    std::memcpy(this->__data._v, vec, 4 * sizeof(FMFLOAT));
-  }
-  vector4(FMFLOAT a, FMFLOAT b, FMFLOAT c, FMFLOAT d) {
-    this->__data[0] = a;
-    this->__data[1] = b;
-    this->__data[2] = c;
-    this->__data[3] = d;
-  }
-
+  vector4(FMFLOAT *vec);
+  vector4(FMFLOAT a, FMFLOAT b, FMFLOAT c, FMFLOAT d);
+  vector4(FMFLOAT a);
   FMFLOAT &FM_CALL operator[](size_t t) { return __data._v[t]; }
+  const FMFLOAT &FM_CALL operator[](size_t t) const { return __data._v[t]; }
 
-  const vector4 FM_INLINE FM_CALL operator+(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+  const vector4 FM_INLINE FM_CALL operator+(const vector4 &b) const noexcept;
 
-    simd::_fm_vec4 ret = simd::fmVecAdd(veca, vecb);
-    return vector4(ret);
-  }
+  const vector4 FM_INLINE FM_CALL operator-(const vector4 &b) const noexcept;
 
-  const vector4 FM_INLINE FM_CALL operator-(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
+  const vector4 FM_INLINE FM_CALL operator*(const vector4 &b) const noexcept;
 
-    simd::_fm_vec4 ret = simd::fmVecSub(veca, vecb);
-    return vector4(ret);
-  }
+  const vector4 FM_INLINE FM_CALL operator/(const vector4 &b) const noexcept;
 
-  const vector4 FM_INLINE FM_CALL operator*(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
-    simd::_fm_vec4 ret = simd::fmVecMul(veca, vecb);
-    return vector4(ret);
-  }
+  const FMFLOAT FM_INLINE FM_CALL dot(const vector4 &b) const noexcept;
 
-  const vector4 FM_INLINE FM_CALL operator/(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
-    simd::_fm_vec4 ret = simd::fmVecDiv(veca, vecb);
-    return vector4(ret);
-  }
+  const vector4 FM_INLINE FM_CALL cross(const vector4 &b) const noexcept;
 
-  const FMFLOAT FM_INLINE FM_CALL dot(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
-    return simd::fmVecDot(veca, vecb);
-  }
+  FMFLOAT FM_INLINE FM_CALL maxElement() const noexcept;
 
-  const vector4 FM_INLINE FM_CALL cross(const vector4 &b) const noexcept {
-    simd::_fm_vec4 veca = simd::fmLoadVecP(__data._v);
-    simd::_fm_vec4 vecb = simd::fmLoadVecP(b.__data._v);
-    simd::_fm_vec4 ret = simd::fmVec3Cross(veca, vecb);
-    return vector4(ret);
-  }
+  FMFLOAT FM_INLINE FM_CALL minElement() const noexcept;
 
-  FMFLOAT FM_INLINE FM_CALL maxElement() const noexcept {
-    simd::_fm_vec4 vec = simd::fmLoadVecP(__data._v);
-    return simd::fmMaxElemOfVec(vec);
-  }
+  FMFLOAT FM_INLINE FM_CALL length() const noexcept;
 
-  FMFLOAT FM_INLINE FM_CALL minElement() const noexcept {
-    simd::_fm_vec4 vec = simd::fmLoadVecP(__data._v);
-    return simd::fmMinElemOfVec(vec);
-  }
+  vector4 FM_INLINE FM_CALL square() const noexcept;
 
-  FMFLOAT FM_INLINE FM_CALL length() const noexcept {
-    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
-    FMFLOAT ff = simd::fmVecDot(vec, vec);
-    return std::sqrt(ff);
-  }
-
-  vector4 FM_INLINE FM_CALL square() const noexcept {
-    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
-    return vector4(simd::fmVecMul(vec, vec));
-  }
-  vector4 FM_INLINE FM_CALL sqrt() const noexcept {
-    simd::_fm_vec4 vec = simd::fmLoadVecP(this->__data._v);
-    return simd::fmVecSqrt(vec);
-  }
+  vector4 FM_INLINE FM_CALL sqrt() const noexcept;
 
   friend const vector4 FM_INLINE FM_CALL operator*(const vector4 &a,
                                                    FMFLOAT b) noexcept;
   friend const vector4 FM_INLINE FM_CALL operator/(const vector4 &a,
                                                    FMFLOAT b) noexcept;
 
-friend std::ostream& operator <<(std::ostream& out,const vector4& s)
-	{
-		out<<"("<<s.__data._v[0]<<","<<s.__data._v[1]<<","<<s.__data._v[2]<<","<<s.__data._v[3]<<")";
-		return out;
-	}
+  friend std::ostream &operator<<(std::ostream &out, const vector4 &s);
 
 protected:
   vector4(const simd::fmAlignFLoat4 &in) : __data(in) {}
@@ -111,18 +55,15 @@ private:
   simd::fmAlignFLoat4 __data;
 };
 
-const vector4 FM_INLINE FM_CALL operator*(const vector4 &a,
-                                          FMFLOAT b) noexcept {
-  simd::_fm_vec4 veca = simd::fmLoadVecP(a.__data._v);
-  simd::_fm_vec4 ret = simd::fmVecScale(veca, b);
-  return vector4(ret);
-}
+const vector4 FM_INLINE FM_CALL operator*(const vector4 &a, FMFLOAT b) noexcept;
 
-const vector4 FM_INLINE FM_CALL operator/(const vector4 &a,
-                                          FMFLOAT b) noexcept {
-  FMFLOAT inv = ((FMFLOAT)1) / b;
-  return a * inv;
-}
+const vector4 FM_INLINE FM_CALL operator*(FMFLOAT b, const vector4 &a) noexcept;
+
+const vector4 FM_INLINE FM_CALL operator/(const vector4 &a, FMFLOAT b) noexcept;
+
+std::ostream &operator<<(std::ostream &out, const vector4 &s);
+
+bool FM_INLINE FM_CALL hasNan(const vector4 &in) noexcept;
 
 } // namespace fm
 
