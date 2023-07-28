5cc791ee1a5b3d1464d7c4bff34c6cb98d220dd8
feiqi3 <zjgtzn@outlook.com>
2023-07-29 01:24:21 +0800

    Some trigonometric functions.

--- 3dOperation/operation.h
+++ 3dOperation/operation.h
@@ -4,50 +4,70 @@
 #include "./constants.h"
 #include <algorithm>
 #include <cstdlib>
+#include <type_traits>
 namespace fm {
 
-	template<typename T>
-	T FM_FORCE_INLINE FM_CALL lerp(T a, T b, FMFLOAT t) FMTHROW {
-		return (1 - t) * a + t * b;
-	}
-	class vector4;
-	vector4 FM_CALL lerp(const vector4& a, const vector4& b, FMFLOAT t)FMTHROW;
+template <typename T>
+T FM_FORCE_INLINE FM_CALL lerp(T a, T b, FMFLOAT t) FMTHROW {
+  return (1 - t) * a + t * b;
+}
+class vector4;
+vector4 FM_CALL lerp(const vector4 &a, const vector4 &b, FMFLOAT t) FMTHROW;
 
-	class vector3;
-	vector3 FM_CALL lerp(const vector3& a, const vector3& b, FMFLOAT t)FMTHROW;
+class vector3;
+vector3 FM_CALL lerp(const vector3 &a, const vector3 &b, FMFLOAT t) FMTHROW;
 
-	template<typename T>
-	T FM_FORCE_INLINE FM_CALL clamp(T a, T min, T max) {
-		return std::min(max, std::max(a, min));
-	}
-
-	vector4 FM_CALL clamp(const vector4& a, const vector4& max,
-		const vector4& min)FMTHROW;
-
-	vector3 FM_CALL clamp(const vector3& a, const vector3& max,
-		const vector3& min)FMTHROW;
-
-	FMFLOAT FM_FORCE_INLINE FM_CALL degToRad(FMFLOAT a)FMTHROW {
-		static FMFLOAT inv180mulPI = FMFLOAT(1) / FMFLOAT(180) * PI;
-		return a * PI * inv180mulPI;
-	}
-
-	FMFLOAT FM_FORCE_INLINE  FM_CALL radToDeg(FMFLOAT a) FMTHROW {
-		static FMFLOAT F180DivPI = FMFLOAT(180) * INV_PI;
-		return a * F180DivPI;
-
-	}
-
-	template <typename T>
-	T FM_FORCE_INLINE FM_CALL abs(T a) {
-		return std::abs(a);
-	}
-
-	vector3 FM_CALL abs(const vector3& vec)FMTHROW;
-
-	vector4 FM_CALL abs(const vector4& vec)FMTHROW;
-
+template <typename T>
+T FM_FORCE_INLINE FM_CALL clamp(T a, T min, T max) FMTHROW {
+  return std::min(max, std::max(a, min));
 }
 
+vector4 FM_CALL clamp(const vector4 &a, const vector4 &max,
+                      const vector4 &min) FMTHROW;
+
+vector3 FM_CALL clamp(const vector3 &a, const vector3 &max,
+                      const vector3 &min) FMTHROW;
+
+FMFLOAT FM_FORCE_INLINE FM_CALL degToRad(FMFLOAT a) FMTHROW {
+  static FMFLOAT inv180mulPI = PI / FMFLOAT(180);
+  return a * inv180mulPI;
+}
+
+FMFLOAT FM_FORCE_INLINE FM_CALL radToDeg(FMFLOAT a) FMTHROW {
+  static FMFLOAT F180DivPI = FMFLOAT(180) * INV_PI;
+  return a * F180DivPI;
+}
+
+template <typename T> T FM_FORCE_INLINE FM_CALL abs(T a) { return std::abs(a); }
+
+vector3 FM_CALL abs(const vector3 &vec) FMTHROW;
+
+vector4 FM_CALL abs(const vector4 &vec) FMTHROW;
+
+FM_CALL double __do_Sin(double x) FMTHROW;
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value,
+                                              bool>::type = true>
+FM_CALL FM_FORCE_INLINE T Sin(T ty) FMTHROW {
+  return __do_Sin(static_cast<double>(ty));
+}
+
+FM_CALL double __do_Cos(double x) FMTHROW;
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value,
+                                              bool>::type = true>
+FM_CALL FM_FORCE_INLINE T Cos(T ty) FMTHROW {
+  return __do_Cos(static_cast<double>(ty));
+}
+
+FM_CALL double __do_Tan(double x) FMTHROW;
+
+template <typename T, typename std::enable_if<std::is_floating_point<T>::value,
+                                              bool>::type = true>
+FM_CALL FM_FORCE_INLINE T Tan(T ty) FMTHROW {
+  return __do_Tan(static_cast<double>(ty));
+}
+
+} // namespace fm
 
 #endif
