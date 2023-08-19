830f51ddac17059240a45b98c0a40136f90f755a
feiqi3 <zjgtzn@outlook.com>
2023-08-20 01:21:43 +0800

    Add test to log2 log10

--- test/logsTest.cxx
+++ test/logsTest.cxx
@@ -14,8 +14,20 @@ int main(){
     std::cout.precision(30);
     std::cout<<std::fixed;
     std::function<double(double)> stdlog = [](double in){return std::log(in);};
+    std::function<double(double)> stdlog2 = [](double in){return std::log2(in);};
+    std::function<double(double)> stdlog10 = [](double in){return std::log10(in);};
     auto premuter = preMute(500000, std::numeric_limits<double>::denorm_min(), 10000000);
     precisionTest(stdlog, fm::Log<double>, "fm::Log", 0.0000001, 1000., 0.0001);
     Timebenchmark(stdlog, "std::log",premuter );
-    Timebenchmark(fm::Log<double>, "fm::log",premuter );
+    Timebenchmark(fm::Log<double>, "fm::Log",premuter );
+
+  std::cout << "<========================>\n\n";
+    precisionTest(stdlog, fm::Log<double>, "fm::Log2", 0.0000001, 1000., 0.0001);
+    Timebenchmark(stdlog2, "std::log2",premuter );
+    Timebenchmark(fm::Log2<double>, "fm::Log2",premuter );
+  std::cout << "<========================>\n\n";
+    precisionTest(stdlog, fm::Log<double>, "fm::Log10", 0.0000001, 1000., 0.0001);
+    Timebenchmark(stdlog2, "std::log10",premuter );
+    Timebenchmark(fm::Log2<double>, "fm::Log10",premuter );
+
 }
